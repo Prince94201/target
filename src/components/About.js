65f450b598152af6ba84fb4db1157ae05c86ffe7
Prince94201 <141738748+Prince94201@users.noreply.github.com>
2024-03-20 12:33:35 +0530

    k

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,6 @@ function About(){
                 <p>The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md </p>
                 <button>Read More</button>
             </div>
-            <>hello</>
         </div>
     )
 }
